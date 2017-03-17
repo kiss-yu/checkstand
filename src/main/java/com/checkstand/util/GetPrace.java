11acca32b6a0acf9b3146db5ee2919f24597eecd
DESKTOP-MV0RSDC\11723 <1172304645@qq.com>
2017-03-17 23:34:32 +0800

    添加支付宝支持

--- src/main/java/com/checkstand/util/GetPrace.java
+++ src/main/java/com/checkstand/util/GetPrace.java
@@ -1,50 +1,44 @@
-package com.checkstand.util;
+package com.checkstand.controller;
 
 /**
  * Created by 11723 on 2017/3/13.
  */
 
-import com.checkstand.controller.SystemController;
 import com.checkstand.service.GoodsService;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
 
 @Component
 public class GetPrace {
     @Resource
     private GoodsService service;
     private Socket socket = null;
+    private float endPrace;
     public  void start() throws IOException {
         ServerSocket serverSocket = new ServerSocket(8888);
-        System.out.println("socket start");
+//        System.out.println("socket start");
         while((socket = serverSocket.accept()) != null){
             try{
                 byte[] bs = new byte[20];
-                String id = "";
-                InetAddress address =socket.getInetAddress();
-                System.out.println("IP:"+address);
+                String content = "";
+//                InetAddress address =socket.getInetAddress();
+//                System.out.println("IP:"+address);
                 InputStream inputStream = socket.getInputStream();
                 int size = inputStream.read(bs);
                 for(int i = 0;i < size;i ++)
-                    id += (char)bs[i];
-                System.out.println("商品id==" + id);
-                OutputStream outputStream = socket.getOutputStream();
-                PrintWriter pWriter = new PrintWriter(outputStream);
-                pWriter.write("#" + service.selectByGoodsId(id).getGoodsPrace() + "@");
-                pWriter.flush();
-                pWriter.close();
-                outputStream.close();
+                    content += (char)bs[i];
+                switch (content){
+                    case "OK":settlement();break;
+                    case "clear":clearPrace();break;
+                    default:statisticalPrace(content);break;
+                }
                 inputStream.close();
                 socket.close();
             }
@@ -53,4 +47,35 @@ public class GetPrace {
             }
         }
     }
+    private void settlement(){
+
+        clearPrace();
+    }
+
+    private void clearPrace(){
+        endPrace = 0;
+        sendData("#" + endPrace + "@");
+    }
+
+    private void statisticalPrace(String id) {
+        endPrace += service.selectByGoodsId(id).getGoodsPrace();
+        System.out.println("商品id==" + id);
+        sendData("#" + endPrace + "@");
+
+    }
+
+    private void sendData(String content) {
+        OutputStream outputStream = null;
+        try {
+            outputStream = socket.getOutputStream();
+            PrintWriter pWriter = new PrintWriter(outputStream);
+            pWriter.write(content);
+            pWriter.flush();
+            pWriter.close();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
